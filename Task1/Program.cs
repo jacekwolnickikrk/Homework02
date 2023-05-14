c5fa6dbb156699ec36faff5ca5a1f258fee3b248
Jacek Wolnicki <jacekwolnicki@gmail.com>
2023-05-14 23:54:07 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\data";
+            string searchPattern = "*.txt";
+            //string[] dirs = Directory.GetFiles(path, searchPattern);
+
+            var files = GetFilesFromFolder(path, searchPattern);
+
+            var wordsInFiles = 0;
+
+            foreach (var file in files)
+            {
+                var readedText = ReadTextInFile(file);
+                var countedWords = CountWordsInText(readedText);
+                wordsInFiles = wordsInFiles + countedWords;
+            }
+
+            Console.WriteLine("Counted words in all files in folder: " + wordsInFiles);
+            Console.ReadKey();
+        }
+
+        static string[] GetFilesFromFolder(string path, string pattern)
+        {
+            return Directory.GetFiles(path, pattern);
+        }
+
+        static string ReadTextInFile(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        static int CountWordsInText(string text)
+        {
+            var splittedText = text.Split(' ');
+            return splittedText.Length;
+        }
+    }
+}
